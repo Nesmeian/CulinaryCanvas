d4659101e47bb7fcaefc4366b56b7ffe33decf2d
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-04-07 22:45:46 +0300

    feat(culinary-blog): complete culinaryBlog section

--- src/components/sections/sectionTextStyles/index.ts
+++ src/components/sections/sectionTextStyles/index.ts
@@ -6,5 +6,9 @@ const sectionTextStyles = {
         paddingRight: '20px',
         lineHeight: '20px',
     },
+    culinaryTextStyles: {
+        fontSize: '14px',
+        letterSpacing: '0.5px',
+    },
 };
 export default sectionTextStyles;
