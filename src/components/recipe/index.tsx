359ae67c452b0052769659a852aab52aae20a1a7
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-04-20 23:04:46 +0300

    feat(recipe): add nutrion section to recipe page

--- src/components/recipe/index.tsx
+++ src/components/recipe/index.tsx
@@ -1,16 +1,19 @@
-import { RecipeData } from '~/types/recipesData/index.ts';
+import { NutritionValueData, RecipeData } from '~/types/recipesData/index.ts';
 
 import DB from '../../data/db.json';
 import MainStyled from '../styledComponents/Main';
-import RecipeCard from './recipeCard.tsx';
+import RecipeCard from './recipeCard';
+import RecipeNutritionValue from './recipenutrition';
 export default function Recipe({ recipe }: { recipe: string }) {
     const recipeItem = DB.recipes.find(({ path }) => recipe === path);
+    const recipenutritionValueData = recipeItem?.nutritionValue;
     if (!recipeItem) {
         return <div>Рецепт не найден</div>;
     }
     return (
-        <MainStyled as='main' pl={{ lg: '14px', md: '20px', sm: '16px' }}>
+        <MainStyled as='main' pl={{ lg: '14px', md: '20px', sm: '16px' }} gap={0}>
             <RecipeCard recipeData={recipeItem as RecipeData} />
+            <RecipeNutritionValue data={recipenutritionValueData as NutritionValueData} />
         </MainStyled>
     );
 }
