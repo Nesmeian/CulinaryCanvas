f8600039b3440aaae574d7dcf7f26d4321f8e0ec
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-05-12 04:51:53 +0300

    feat(route): add direct to login page

---
+++ src/components/Pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import { HStack, Image, Text, VStack } from '@chakra-ui/react';
+
+export const Login = () => (
+    <HStack>
+        <VStack>
+            <Text>Jac</Text>
+        </VStack>
+        <Image />
+    </HStack>
+);
