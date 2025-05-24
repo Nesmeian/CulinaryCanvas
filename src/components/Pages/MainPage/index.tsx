75afc2e3192f98e8270e21593cf4c0f6c4e65010
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-05-24 16:31:50 +0300

    feat(newRecipe): remove sideBar when user on new-recipePage

--- src/components/Pages/MainPage/index.tsx
+++ src/components/Pages/MainPage/index.tsx
@@ -6,16 +6,19 @@ import Header from '~/components/Header';
 import NavMenu from '~/components/navMenu';
 import Sidebar from '~/components/Sidebar';
 import useBreakpoints from '~/themes/chakraBreakPoints';
+import GetCurrentPath from '~/utils/getCurrentPath';
 
 export const MainPage = () => {
     const { isTablet } = useBreakpoints();
+    const currentPath = GetCurrentPath();
+    const newRecipePath = currentPath[0] === 'new-recipe';
     return (
         <Stack gap='0px'>
             <Header />
             <HStack className='app' alignItems='flex-start' gap='12px' justify='space-between'>
                 {!isTablet && <NavMenu />}
                 <Outlet />
-                {!isTablet && <Sidebar />}
+                {!isTablet && !newRecipePath && <Sidebar />}
             </HStack>
             <Footer />
         </Stack>
