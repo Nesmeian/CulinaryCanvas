d422a853d2b51c15a196b18f23491b68449af617
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-05-26 17:10:11 +0300

    feat(newRecipe): add validation and types for time and portions inputs

--- src/components/NewRecipeComponents/personCount/index.tsx
+++ src/components/NewRecipeComponents/personCount/index.tsx
@@ -1,12 +1,13 @@
 import { Heading, HStack } from '@chakra-ui/react';
 
 import { NumberInput } from '~/components/NumberInput';
+import { RecipeFormHelpers } from '~/types/NewRecipesTypes';
 
 import { newRecipeHeadingStyle } from '../componentStyles';
 
-export const PersonCount = () => (
+export const PersonCount = ({ errors, setValue }: RecipeFormHelpers) => (
     <HStack gap='24px'>
         <Heading {...newRecipeHeadingStyle}>На сколько человек ваш рецепт?</Heading>
-        <NumberInput defaultValue={4} />
+        <NumberInput value={4} name='portions' errors={errors} setValue={setValue} />
     </HStack>
 );
