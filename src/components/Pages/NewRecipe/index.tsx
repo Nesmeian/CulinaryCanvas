8db101f7cec66d3da51726629beac059d81ad492
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-05-26 12:56:23 +0300

    feat(newRecipe): implement drop modal for images

--- src/components/Pages/NewRecipe/index.tsx
+++ src/components/Pages/NewRecipe/index.tsx
@@ -1,13 +1,17 @@
-import { HStack, Image } from '@chakra-ui/react';
+import { HStack, Image, useDisclosure } from '@chakra-ui/react';
 import { chakra } from '@chakra-ui/react';
+import { useState } from 'react';
 
+import { DropImageModal } from '~/components/dropImageModal';
 import { RecipeBuilder } from '~/components/NewRecipeComponents/RecipeBuilder';
 import { RecipeMainInf } from '~/components/NewRecipeComponents/RecipeMainInf';
 import MainStyled from '~/components/styledComponents/Main';
 
 import emptyImg from '../../../assets/emptyImage.png';
+import { RecipeImgStyles } from './styles';
 export const NewRecipe = () => {
-    console.log('jack');
+    const { isOpen, onClose, onOpen } = useDisclosure();
+    const [recipeImg, setRecipeImage] = useState(emptyImg);
     return (
         <MainStyled as='main'>
             <HStack mt='56px' w='100%'>
@@ -19,12 +23,18 @@ export const NewRecipe = () => {
                     alignItems='center'
                 >
                     <HStack alignItems='flex-start' gap={5} h='410px' w='100%'>
-                        <Image src={emptyImg} alt='recipeImg' />
+                        <Image {...RecipeImgStyles} src={recipeImg} onClick={onOpen} />
                         <RecipeMainInf />
                     </HStack>
                     <RecipeBuilder />
                 </chakra.form>
             </HStack>
+            <DropImageModal
+                isOpen={isOpen}
+                initImage={recipeImg}
+                onClose={onClose}
+                setRecipeImage={setRecipeImage}
+            />
         </MainStyled>
     );
 };
---
+++ src/components/Pages/NewRecipe/styles.ts
@@ -0,0 +1,7 @@
+export const RecipeImgStyles = {
+    objectFit: 'cover',
+    height: { lg: '410px', base: '224px' },
+    width: { xl: '553px', lg: '353px', md: '232px', base: '328px ' },
+    cursor: 'pointer',
+    alt: 'recipeImg',
+} as const;
