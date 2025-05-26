53d0a01881403a3e8d529913cf21e80de47894af
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-05-26 12:56:23 +0300

    feat(newRecipe): implement drop modal for images

---
+++ src/components/dropImageModal/index.tsx
@@ -0,0 +1,60 @@
+import { Box, Button, Center, Heading, Image, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+import { dropImageModalHeader, dropImageModalWrapperStyles } from './styles';
+
+export const DropImageModal = ({
+    isOpen,
+    onClose,
+    initImage,
+    setRecipeImage,
+}: {
+    isOpen: boolean;
+    onClose: () => void;
+    initImage: string;
+    setRecipeImage: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+    const [preview, setPreview] = useState<string>(initImage);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: { 'image/*': [] },
+        multiple: false,
+        onDrop: ([file]) => setPreview(URL.createObjectURL(file)),
+    });
+    const saveImageHandler = () => {
+        setRecipeImage(preview);
+        onClose();
+    };
+    if (!isOpen) return null;
+
+    return (
+        <Center {...dropImageModalWrapperStyles}>
+            <VStack bg='white' p='32px' spacing='16px' textAlign='center'>
+                <Heading {...dropImageModalHeader}>Изображение</Heading>
+                <Box
+                    {...getRootProps()}
+                    border='2px dashed'
+                    borderRadius='md'
+                    cursor='pointer'
+                    _hover={{ opacity: 0.8 }}
+                >
+                    <input {...getInputProps()} />
+
+                    <Image
+                        src={preview}
+                        alt='drop-zone preview'
+                        boxSize='206px'
+                        objectFit='contain'
+                    />
+                </Box>
+
+                {preview !== initImage && (
+                    <Button variant='commonLoginBtn' onClick={saveImageHandler}>
+                        Сохранить
+                    </Button>
+                )}
+            </VStack>
+        </Center>
+    );
+};
---
+++ src/components/dropImageModal/styles.ts
@@ -0,0 +1,18 @@
+export const dropImageModalWrapperStyles = {
+    h: '100vh',
+    w: '100vw',
+    bg: 'rgba(0, 0, 0, 0.7)',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 9999,
+} as const;
+export const dropImageModalHeader = {
+    as: 'h2',
+    size: 'h2',
+    fontSize: '24px',
+    lineHeight: '32px',
+    fontWeight: '700',
+    textAlign: 'center',
+    letterSpacing: '0.5px',
+} as const;
