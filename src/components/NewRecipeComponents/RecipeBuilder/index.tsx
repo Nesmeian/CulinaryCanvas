79a0d8a707084584d636d1ad610d3cdb4a60f6d7
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-05-26 14:19:03 +0300

    feat(newRecipe): put image into form data

--- src/components/NewRecipeComponents/RecipeBuilder/index.tsx
+++ src/components/NewRecipeComponents/RecipeBuilder/index.tsx
@@ -1,4 +1,4 @@
-import { VStack } from '@chakra-ui/react';
+import { Button, VStack } from '@chakra-ui/react';
 
 import { IngredientsBlock } from '../IngredientsBlock';
 import { RecipeStepsBlock } from '../recipeStepsBlock';
@@ -7,5 +7,8 @@ export const RecipeBuilder = () => (
     <VStack w={{ lg: '658px', md: '604px', base: '328px' }} mt={{ base: '44px' }}>
         <IngredientsBlock />
         <RecipeStepsBlock />
+        <Button type='submit' variant='commonLoginBtn'>
+            Опубликовать рецепт
+        </Button>
     </VStack>
 );
--- src/components/Pages/NewRecipe/index.tsx
+++ src/components/Pages/NewRecipe/index.tsx
@@ -1,21 +1,32 @@
 import { HStack, Image, useDisclosure } from '@chakra-ui/react';
 import { chakra } from '@chakra-ui/react';
 import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { DropImageModal } from '~/components/dropImageModal';
 import { RecipeBuilder } from '~/components/NewRecipeComponents/RecipeBuilder';
 import { RecipeMainInf } from '~/components/NewRecipeComponents/RecipeMainInf';
 import MainStyled from '~/components/styledComponents/Main';
+import { RecipeFields } from '~/types/NewRecipesTypes';
 
 import emptyImg from '../../../assets/emptyImage.png';
 import { RecipeImgStyles } from './styles';
+
 export const NewRecipe = () => {
     const { isOpen, onClose, onOpen } = useDisclosure();
     const [recipeImg, setRecipeImage] = useState(emptyImg);
+    const { handleSubmit, setValue } = useForm<RecipeFields>({ mode: 'onChange' });
+    const onSubmit: SubmitHandler<RecipeFields> = (data) => {
+        console.log(data, 'data');
+    };
+    const handleImageSave = (file: File) => {
+        setValue('image', file);
+    };
     return (
         <MainStyled as='main'>
             <HStack mt='56px' w='100%'>
                 <chakra.form
+                    onSubmit={handleSubmit(onSubmit)}
                     w='calc(100% - 256px)'
                     display='flex'
                     flexDir='column'
@@ -34,6 +45,7 @@ export const NewRecipe = () => {
                 initImage={recipeImg}
                 onClose={onClose}
                 setRecipeImage={setRecipeImage}
+                setFormImage={handleImageSave}
             />
         </MainStyled>
     );
