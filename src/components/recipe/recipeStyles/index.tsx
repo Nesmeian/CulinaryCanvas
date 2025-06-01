13aae52c30be37d6778b33ccc5696c19411e7b3b
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-06-01 14:28:42 +0300

    feat(deleteRecipe): implement delete recipe on click

--- src/components/recipe/recipeStyles/index.tsx
+++ src/components/recipe/recipeStyles/index.tsx
@@ -6,3 +6,23 @@ export const stepTextStyle = {
     letterSpacing: '0.5px',
     lineHeight: '20px',
 };
+export const commonRecipeBtnStyles = {
+    fontSize: { xl: '18px', lg: '14', sm: '12px' },
+    letterSpacing: '0.5px',
+    className: 'card__btn-save',
+    border: '1px solid rgba(0, 0, 0, 0.48)',
+    backgroundColor: 'white',
+    p: { xl: '14px 24px', lg: '0 12px', sm: '0 6px' },
+    size: {
+        base: 'xs',
+        lg: 'sm',
+        xl: 'xl',
+    },
+};
+export const greenRecipeBtnStyles = {
+    fontSize: { xl: '18px', lg: '14px', sm: '12px' },
+    letterSpacing: '0.5px',
+    p: { xl: '14px 24px', lg: '0 12px', sm: '0 6px' },
+    className: 'card__btn-cook',
+    background: '#B1FF2E',
+};
---
+++ src/query/services/delete/deleteRecipe.ts
@@ -0,0 +1,25 @@
+import { ApiEndpoints } from '~/query/constants/api.ts';
+import { ApiGroupNames } from '~/query/constants/api-group-names.ts';
+import { EndpointNames } from '~/query/constants/endpoint-names.ts';
+import { Tags } from '~/query/constants/tags.ts';
+import { apiSlice } from '~/query/create-api.ts';
+
+export const deleteRecipeApi = apiSlice
+    .enhanceEndpoints({
+        addTagTypes: [Tags.RECIPES],
+    })
+    .injectEndpoints({
+        endpoints: (builder) => ({
+            deleteRecipe: builder.mutation<void, string>({
+                query: (payload) => ({
+                    url: `${ApiEndpoints.RECIPES}/${payload}`,
+                    method: 'DELETE',
+                    apiGroupName: ApiGroupNames.RECIPES,
+                    name: EndpointNames.GET_RECIPES,
+                }),
+                invalidatesTags: [Tags.AUTH_TOKEN],
+            }),
+        }),
+    });
+
+export const { useDeleteRecipeMutation } = deleteRecipeApi;
