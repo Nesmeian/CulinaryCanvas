b6ba5e65643156e7fb373ad0cb9e9571ad3cc4c3
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2025-07-06 12:45:10 +0300

    docs(deploy): set vite.config for deploy"

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }) => ({
-    base: command === 'build' ? '/Nesmeian/' : '/',
+export default defineConfig(() => ({
+    base: '/CulinaryCanvas/',
     plugins: [react()],
+    build: {
+        outDir: 'build',
+    },
     server: {
         host: true,
         port: 3000,
